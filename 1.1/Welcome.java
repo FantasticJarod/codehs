2b420e8f52edd3032ab1ed1085f2d0059a0e754f
Jack Spencer <UniformKitten281@gmail.com>
2022-09-12 08:23:07 -0700

    Create Welcome.java

---
+++ 1.1/Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Jack.");
+        System.out.println("My favorite hobby is programming.");
+    } 
+}
