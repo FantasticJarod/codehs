4f43514737396a21d54bce49660ef6ffcf8e3123
Jack Spencer <UniformKitten281@gmail.com>
2022-09-12 08:25:23 -0700

    Create Timeline.java

---
+++ 1.1/Timeline.java
@@ -0,0 +1,25 @@
+public class Timeline
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Age   | M/YY - Description");
+        System.out.println("6     | 2/13 - Got my first bicycle");
+        printTabs();
+        System.out.println("8     | 4/15 - Turned 8");
+        printTabs();
+        System.out.println("10    | 9/17 - First Day of 5th grade");
+        printTabs();
+        System.out.println("12    | 9/19 - First Day of 7th Grade");
+        printTabs();
+        System.out.println("14    | 9/21 - First Day of 9th Grade");
+        printTabs();
+        System.out.println("16    | 9/23 - First day of 11th grade");
+    }
+    
+    private static void printTabs()
+    {   
+        System.out.println("      |");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
