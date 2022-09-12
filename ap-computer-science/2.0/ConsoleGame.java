a098f045bc630a8ef08b5adfadfc1c9397fa4fa5
Jack Spencer <UniformKitten281@gmail.com>
2022-09-12 13:06:10 -0700

    Create ConsoleGame.java

---
+++ ap-computer-science/2.0/ConsoleGame.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * The ConsoleGame class represents a console game.
+ * 
+ * Add the instance variables you think a console game would need
+ */ 
+
+public class ConsoleGame
+{
+    private String name;
+    private String weapon;
+    private int level;
+    private int xp;
+    private int health;
+    private int maxhealth;
+    private int attack;
+    private int defense;
+    private int speed;
+}
