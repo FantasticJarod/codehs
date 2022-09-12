92b58ea9b87999eec025e783be0dd02277f14ae8
Jack Spencer <UniformKitten281@gmail.com>
2022-09-12 08:31:36 -0700

    Create Pyramid.java

---
+++ 1.3/Pyramid.java
@@ -0,0 +1,10 @@
+public class Pyramid 
+{
+    public static void main(String[] args)
+    {
+        final double blockWeight = 2.5;
+        final int numBlocks = 2500000;
+        
+        System.out.println("The pyramid weights " + numBlocks * blockWeight + " tons");
+    }
+}
