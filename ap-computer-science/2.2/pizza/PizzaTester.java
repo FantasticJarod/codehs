157e0ee3e2b3b6be919aaf9ffe827edfb4443cb9
Jack Spencer <UniformKitten281@gmail.com>
2022-09-13 08:00:21 -0700

    Create PizzaTester.java

---
+++ ap-computer-science/2.2/pizza/PizzaTester.java
@@ -0,0 +1,12 @@
+public class PizzaTester 
+{
+    public static void main(String[] args)
+    {
+        Pizza pizza = new Pizza("", "", 0);
+        Pizza pizza2 = new Pizza("", "", 0);
+        Pizza pizza3 = new Pizza("", "", 0);
+        System.out.println(pizza);
+        System.out.println(pizza2);
+        System.out.println(pizza3);
+    }
+}
