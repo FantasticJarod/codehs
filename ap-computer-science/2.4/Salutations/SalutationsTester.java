70f134ea478e538c1945d49c4debab1e2f8fe0af
Jack Spencer <UniformKitten281@gmail.com>
2022-09-19 07:57:07 -0700

    Create SalutationsTester.java

---
+++ ap-computer-science/2.4/Salutations/SalutationsTester.java
@@ -0,0 +1,11 @@
+public class SalutationsTester
+{
+    public static void main(String[] args)
+    {
+        Salutations letter = new Salutations("Jack");
+        letter.addressLetter();
+        letter.signLetter();
+        letter.addressMemo();
+        letter.signMemo();
+    }
+}
