8951366b85a12f60c94b2368c2990558f3e87bc8
Jack Spencer <UniformKitten281@gmail.com>
2022-10-20 13:11:55 -0700

    Create Circle.java

---
+++ ap-computer-science/2.10/Circle/Circle.java
@@ -0,0 +1,32 @@
+public class Circle
+{
+    private double radius;
+    
+    public Circle(double theRadius)
+    {
+        radius = theRadius;
+    }
+
+    // Implement getArea using
+    // Math.PI and
+    // Math.pow
+    // Area = pi * r^2
+    public double getArea()
+    {
+        double area = radius * radius * Math.PI;
+        return area;
+    }
+    
+    // Implement getSphereVolume using
+    // Math.PI and
+    // Math.pow
+    // Volume = 4/3 * pi * r^3
+    public double getSphereVolume()
+    {
+        double volume = (4.0 / 3.0) * radius * radius * radius * Math.PI;
+        return volume;
+    }
+    
+    
+    
+}
