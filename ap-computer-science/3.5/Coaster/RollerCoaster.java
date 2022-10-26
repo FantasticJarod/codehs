ddf97bf8fedc6da12882f256c918aa8e8867c90d
Jack Spencer <UniformKitten281@gmail.com>
2022-10-26 12:41:37 -0700

    Update RollerCoaster.java

--- ap-computer-science/3.5/Coaster/RollerCoaster.java
+++ ap-computer-science/3.5/Coaster/RollerCoaster.java
@@ -4,14 +4,17 @@ public class RollerCoaster
 {
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);      
-        System.out.println("How old are you?");
-        int age = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);   
         
         System.out.println("How tall are you?");
         int height = scanner.nextInt();
         
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+        
         if (age > 8 && height > 41) {
+            System.out.println("Welcome aboard!");
+        } else if (age == 9 && height == 42) {
             System.out.println("Welcome aboard!");
         } else {
             System.out.println("Sorry, you are not eligible to ride");
