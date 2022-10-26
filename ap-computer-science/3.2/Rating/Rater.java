14349496d0e3376b433b23dd3106ff7e675d4f49
Jack Spencer <UniformKitten281@gmail.com>
2022-10-26 08:58:48 -0700

    Create Rater.java

---
+++ ap-computer-science/3.2/Rating/Rater.java
@@ -0,0 +1,40 @@
+public class Rater
+{
+    private String name; // name of company
+    private double rating; // number rating (1 - 5)
+    
+    
+    public Rater(String company, double initialRating)
+    {
+        name = company;
+        rating = initialRating;
+    }
+    
+    // Set rating to newRating
+    // As long as it's no more than 5
+    public void setRating(double newRating)
+    {
+        if (newRating <= 5.0) {
+            rating = newRating;
+        }
+    }
+    
+    // Returns the rating of the company
+    public double getRating()
+    {
+        return rating;
+    }
+    
+    // Returns a string representation of the company
+    // based on their ratings
+    public String toString()
+    {
+        if (rating < 2.0) {
+            return "Not Recommended Company " + name;
+        } else if (rating > 3.5) {
+            return "Gold Star Company " + name;
+        } else {
+            return "Well Rated Company " + name;
+        }
+    }
+}
