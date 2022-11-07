5df75b92f4c75c8f7c4a53cea3c7215f5c7b64cc
Jack Spencer <UniformKitten281@gmail.com>
2022-11-07 13:21:38 -0800

    Create Odds.java

---
+++ ap-computer-science/4.2/Odds/Odds.java
@@ -0,0 +1,11 @@
+public class Odds
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            } 
+        }
+    }
+}
