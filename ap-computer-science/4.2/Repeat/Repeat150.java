deb24b06735d3573845750339b57bf162bdcd444
Jack Spencer <UniformKitten281@gmail.com>
2022-11-07 13:23:18 -0800

    Create Repeat150.java

---
+++ ap-computer-science/4.2/Repeat/Repeat150.java
@@ -0,0 +1,9 @@
+public class Repeat150
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 150; i++) {
+            System.out.println("Are we there yet?");
+        }
+    }
+}
