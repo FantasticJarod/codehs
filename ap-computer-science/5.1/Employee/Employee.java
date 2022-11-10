16566b151b04383ba12ca22046734b78d09bb388
Jack Spencer <UniformKitten281@gmail.com>
2022-11-10 09:22:43 -0800

    Create Employee.java

---
+++ ap-computer-science/5.1/Employee/Employee.java
@@ -0,0 +1,14 @@
+public class Employee
+{
+    private String name;
+    private String lastName;
+    private double salary;
+    private int hours;
+    
+    public Employee(String xname, String clast, double xsalary, int xhours) {
+        name = xname;
+        lastName = clast;
+        salary = xsalary;
+        hours = xhours;
+    }
+}
